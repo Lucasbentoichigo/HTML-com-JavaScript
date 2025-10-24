ef49c6ff78921b9a0ccedf3f207f2ff4aa74b183
lucas <lucastavaresbento@gmail.com>
2025-10-24 11:09:21 -0300

    first commit

---
+++ exemplo-01/assets/js/script.js
@@ -0,0 +1,20 @@
+// Selecione os elementos
+const botao = document.getElementById("botao");
+const titulo = document.getElementById("titulo");
+const body = document.body;
+
+// Adiciona o evento de clique
+botao.addEventListener("click", () => {
+    // Verifica se o modo atual é claro
+    if (body.classList.contains("claro")) {
+        // Troca para o modo escuro
+        body.classList.replace("claro", "escuro");
+        titulo.textContent = "Modo Escuro 🌙";
+        botao.textContent = "Ativar modo claro ☀️";
+    } else {
+        // Troca para o modo claro
+        body.classList.replace("escuro", "claro");
+        titulo.textContent = "Modo Claro ☀️";
+        botao.textContent = "Ativar modo escuro 🌙"
+    }
+});
